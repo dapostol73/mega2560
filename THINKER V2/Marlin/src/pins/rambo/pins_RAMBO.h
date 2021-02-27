4aa9487e1a0d3d7cea8af117f2ed7e71e36c1c82
Demetrius Apostolopoulos <75643490+dapostol73@users.noreply.github.com>
2021-02-27 13:26:23 -0800

    A12 Eryone Thinker Custom settings
    First submit of Eryone Thinker custom setting for current setup with BL Touch, Filament Runout, Custom name and predefined steps for steppers and extruder.

--- THINKER V2/Marlin/src/pins/rambo/pins_RAMBO.h
+++ THINKER V2/Marlin/src/pins/rambo/pins_RAMBO.h
@@ -56,7 +56,7 @@
 #define Y_MIN_PIN          11
 #define Y_MAX_PIN          23
 #define Z_MAX_PIN          30
-//#define FIL_RUNOUT_PIN     10//24
+#define FIL_RUNOUT_PIN     10//24
 
 
 #if ENABLED(BLTOUCH)
@@ -165,7 +165,7 @@
   #define FAN_PIN           8
 #endif
 #define FAN1_PIN            2
-#define FAN2_PIN          6  
+#define FAN2_PIN            6  
 
 //
 // Misc. Functions
